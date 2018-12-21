d74ef16091df3ad4a7e9ad8b73c9e15c55537fbd
KimHyeWonn <hyewonkim96@skuniv.ac.kr>
2018-12-22 02:17:06 +0900

    second commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import queryString from 'query-string';
+const About = ({location,match}) => {
+    const query = queryString.parse(location.search);
+    const detail = query.detail === 'true';
 
-const About = () => {
     return (
         <div>
-            <h2>About</h2>
+            <h2>About {match.params.name}</h2>
+            {detail && 'detail: blahblah'}
         </div>
     );
 };
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Home from 'pages';
-import About from 'pages/About';
-
+import { Route,Switch } from 'react-router-dom';
+import {Home,About,Posts} from 'pages';
+import Menu from 'components/Menu';
 class App extends Component {
     render() {
         return (
             <div>
+                <Menu/>
                  <Route exact path="/" component={Home}/>
-                <Route path="/about" component={About}/>
+                 <Switch>
+                    <Route path="/about/:name" component={About}/>
+                    <Route path="/about" component={About}/>
+                 </Switch>
+                 <Route path="/posts" component={Posts}/>
             </div>
         );
     }
 }
-
+//
 export default App;
